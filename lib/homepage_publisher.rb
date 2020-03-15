ec5f83ce3f8c152141be7b135f71c3f83629dbef
Kevin Dew <kevindew@me.com>
2020-03-15 15:32:13 +0000

    Remove unnecessary quotes
    Because of the colons these strings will all have been converted to
    symbols.

--- lib/homepage_publisher.rb
+++ lib/homepage_publisher.rb
@@ -4,32 +4,32 @@ module HomepagePublisher
 
   def self.homepage_content_item
     {
-      "base_path": "/",
-      "document_type": "homepage",
-      "schema_name": "homepage",
-      "title": "GOV.UK homepage",
-      "description": "",
-      "locale": "en",
-      "details": {
+      base_path: "/",
+      document_type: "homepage",
+      schema_name: "homepage",
+      title: "GOV.UK homepage",
+      description: "",
+      locale: "en",
+      details: {
       },
-      "routes": [
+      routes: [
         {
-          "path": "/",
-          "type": "exact",
+          path: "/",
+          type: "exact",
         },
       ],
-      "publishing_app": "frontend",
-      "rendering_app": "frontend",
-      "public_updated_at": Time.zone.now.iso8601,
-      "update_type": "major",
+      publishing_app: "frontend",
+      rendering_app: "frontend",
+      public_updated_at: Time.zone.now.iso8601,
+      update_type: "major",
     }
   end
 
   def self.links
     {
       links: {
-        "organisations" => [GDS_ORGANISATION_CONTENT_ID],
-        "primary_publishing_organisation" => [GDS_ORGANISATION_CONTENT_ID],
+        organisations: [GDS_ORGANISATION_CONTENT_ID],
+        primary_publishing_organisation: [GDS_ORGANISATION_CONTENT_ID],
       },
     }
   end
