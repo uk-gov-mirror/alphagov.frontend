546869fdae5622d33dadc20b0945663a27185657
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-07-16 19:27:05 +0100

    Set statsd logger level

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,9 @@ module Frontend
 
     config.active_job.queue_adapter = :sidekiq
 
+    Statsd.logger = Logger.new(STDOUT)
+    Statsd.logger.level = Logger::DEBUG
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
