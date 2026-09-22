7132ef3ff5ec7b89fabc435354f1a80c1a982bb2
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-22 15:15:53 +0100

    Move document feed / share sections from model to layout

--- spec/models/topical_event_spec.rb
+++ spec/models/topical_event_spec.rb
@@ -21,61 +21,6 @@ RSpec.describe TopicalEvent do
 
   before { stub_any_search.to_return(body: search_results.to_json) }
 
-  describe "feed initialisation" do
-    it "creates a DocumentList with appropriate settings" do
-      expect(FlexiblePage::FlexibleSection::DocumentList).to receive(:new).with(
-        email_signup_link: "/email-signup?link=#{content_store_response['base_path']}",
-        email_signup_link_text: "Get email updates",
-        heading_text: "Latest updates",
-        items: [{
-          link: {
-            path: "/news/my-item",
-            text: "My Topical Event News Item",
-          },
-          metadata: {
-            document_type: "News",
-            public_updated_at: Time.zone.parse("2025-12-01 00:00:01.000000000 +0000"),
-            display_type: "news",
-            description: "What's up?",
-          },
-        }],
-        see_all_items_link: "/search/all?order=updated-newest&topical_events%5B%5D=western-balkans-summit-london-2018",
-        see_all_items_link_text: "See more updates",
-      )
-
-      topical_event
-    end
-  end
-
-  describe "social initialisation" do
-    it "creates a Share with appropriate settings" do
-      expect(FlexiblePage::FlexibleSection::Share).to receive(:new).with(
-        links: [{
-          href: "https://twitter.com/foreignoffice",
-          icon: "twitter",
-          text: "Twitter",
-        }],
-        heading_text: "Follow us",
-      )
-
-      topical_event
-    end
-
-    context "when there are no social_media_links" do
-      let(:content_store_response) do
-        GovukSchemas::Example.find("topical_event", example_name: "western-balkans-summit-london-2018").tap do |item|
-          item["details"]["social_media_links"] = nil
-        end
-      end
-
-      it "does not create a Share" do
-        expect(FlexiblePage::FlexibleSection::Share).not_to receive(:new)
-
-        topical_event
-      end
-    end
-  end
-
   describe "who's involved initialisation" do
     it "creates an Involved with appropriate settings" do
       expect(FlexiblePage::FlexibleSection::Involved).to receive(:new) do |settings, _|
