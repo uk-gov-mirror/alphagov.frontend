57ad22aa110cf3351090e763f771dd789a4fe6b7
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-22 15:30:07 +0100

    Move Involved section from Model to view

--- spec/models/topical_event_spec.rb
+++ spec/models/topical_event_spec.rb
@@ -21,31 +21,6 @@ RSpec.describe TopicalEvent do
 
   before { stub_any_search.to_return(body: search_results.to_json) }
 
-  describe "who's involved initialisation" do
-    it "creates an Involved with appropriate settings" do
-      expect(FlexiblePage::FlexibleSection::Involved).to receive(:new) do |settings, _|
-        expect(settings[:organisations].first).to be_a(Organisation)
-        expect(settings[:organisations].first.title).to eq(content_store_response["links"]["organisations"][0]["title"])
-      end
-
-      topical_event
-    end
-
-    context "when there are no organisations" do
-      let(:content_store_response) do
-        GovukSchemas::Example.find("topical_event", example_name: "western-balkans-summit-london-2018").tap do |item|
-          item["links"]["organisations"] = nil
-        end
-      end
-
-      it "doesn't create an Involved" do
-        expect(FlexiblePage::FlexibleSection::Involved).not_to receive(:new)
-
-        topical_event
-      end
-    end
-  end
-
   describe "#header_image" do
     let(:content_store_response) { GovukSchemas::Example.find("topical_event", example_name:) }
     let(:example_name) { "topical-event" }
